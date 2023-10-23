205437b87aff9480d8ec25a0299ceb1d503856ab
HenryGohYX <HenryGohYX@gmail.com>
2023-10-24 00:29:45 +0800

    Navigate to repo directly for convenience

--- tech/my-app/src/AppNavbar.js
+++ tech/my-app/src/AppNavbar.js
@@ -13,7 +13,7 @@ const AppNavbar = () => {
       <Collapse isOpen={isOpen} navbar>
         <Nav className="justify-content-end" style={{width: "100%"}} navbar>
           <NavItem>
-            <NavLink href="https://github.com/HenryGohYX">GitHub</NavLink>
+            <NavLink href="https://github.com/HenryGohYX/LunchSuggestion">GitHub</NavLink>
           </NavItem>
         </Nav>
       </Collapse>
